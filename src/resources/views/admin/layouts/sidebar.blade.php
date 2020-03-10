64f0bfcc3c5b315ced5fab4d26a6ff66704445ae
sramitroy <uamitroy@gmail.com>
2020-03-10 20:14:19 +0530

    url issues

--- src/resources/views/admin/layouts/sidebar.blade.php
+++ src/resources/views/admin/layouts/sidebar.blade.php
@@ -24,26 +24,21 @@
     </li>
 
   <li class="nav-item">
-    <a class="nav-link" href="{{ route('admin.players.index') }}">
+    <a class="nav-link" href="#">
       <i class="fas fa-fw fa-users"></i>
       <span>Players</span></a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="{{ route('admin.agents.index') }}">
+    <a class="nav-link" href="#">
       <i class="fas fa-fw fa-users"></i>
       <span>Agents</span></a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="{{ route('admin.plans.index') }}">
+    <a class="nav-link" href="#">
       <i class="fab fa-product-hunt"></i>
       <span>Plans</span></a>
   </li>
-  <!-- <li class="nav-item">
-    <a class="nav-link" href="{{ route('admin.playerstatistics.index') }}">
-      <i class="far fa-chart-bar"></i>
-      <span>Player Statistics</span></a>
-  </li> -->
-  
+ 
     <li class="nav-item">
         <a class="nav-link collapsed" href="javascript:;" data-toggle="collapse" data-target="#collapseOfferings" aria-expanded="true" aria-controls="collapsePages">
           <i class="fa fa-cog"></i>
@@ -51,32 +46,22 @@
         </a>
         <div id="collapseOfferings" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar" style="">
           <div class="bg-white py-2 collapse-inner rounded">
-            <a class="collapse-item" href="{{ route('admin.countries.index') }}">Countries</a>
-            <a class="collapse-item" href="{{ route('admin.states.index') }}">States</a>
-            <a class="collapse-item" href="{{ route('admin.cities.index') }}">Cities</a>
-            <a class="collapse-item" href="{{ route('admin.statistics.index') }}">Statistics</a>
-            <a class="collapse-item" href="{{ route('admin.positions.index') }}">Positions</a>
+            <a class="collapse-item" href="#">Countries</a>
+            <a class="collapse-item" href="#">States</a>
+            <a class="collapse-item" href="#">Cities</a>
+            <a class="collapse-item" href="#">Statistics</a>
+            <a class="collapse-item" href="#">Positions</a>
           </div>
         </div>
       </li>
 
-      <!-- <li class="nav-item">
-        <a class="nav-link" href="{{ route('admin.banners.index') }}">
-        <i class="fas fa-fw fa-image"></i>
-        <span>Banners</span></a>
-      </li> -->
-      <!-- <li class="nav-item">
-        <a class="nav-link" href="{{ route('admin.pages.index') }}">
-        <i class="fas fa-fw fa-list"></i>
-        <span>Pages</span></a>
-     </li> -->
      <li class="nav-item">
-      <a class="nav-link" href="{{ route('admin.reports.contact') }}">
+      <a class="nav-link" href="#">
       <i class="fas fa-id-card"></i>
       <span>Contact Us</span></a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="{{ route('admin.reports.newsletters') }}">
+      <a class="nav-link" href="#">
       <i class="fas fa-fw fa-rss"></i>
       <span>Newsletters</span></a>
     </li>
